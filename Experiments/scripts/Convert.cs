ddd7e15c3dd8a245e1efc4d9a76f2a3432e53561
Phattharathorn Dararattanaroj <pan_pattaratorn@hotmail.com>
2019-07-24 17:54:11 +0700

    Task 2: convert json v.1

---
+++ Experiments/scripts/Convert.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Convert : MonoBehaviour
+{
+    string path = "D:/TestCode/map1_2x2.json";
+    string textString;
+
+    void Start()
+    {
+        textString = File.ReadAllText(path);
+        Debug.Log(textString);
+        MapData mapData = JsonUtility.FromJson<MapData>(textString);
+        string mData = JsonUtility.ToJson(mapData, true);
+        Debug.Log(mData);
+    }
+}
+
+[System.Serializable]
+public class MapData
+{
+    public string id;
+    public int[] position;
+    public string properties;
+    public int[] parameters;
+}
